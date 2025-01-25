c1b44ac208ac5e0bb3f73505e353535fb5d78e3c
Benjamingainyll <joelbenjamin.tech@gmail.com>
2025-01-25 14:22:10 +0200

    first commit of files

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+              <p>Welcome to Kulture Essence, where tradition meets style. We bring authentic Nigerian men’s wear to Kigali, celebrating the elegance and heritage of our culture.</p>
+              <p>Our clothing is sourced from skilled artisans in Northern Nigeria, ensuring each piece reflects quality, comfort, and timeless design. With a focus on affordability and convenience, we provide ready-made garments and reliable delivery options for a seamless shopping experience.
+
+</p>
+<p>At Kulture Essence, we’re proud to share the beauty of Nigerian fashion with the world.</p>
+              <b className='text-gray-800'>Our Mission</b>
+              <p>Our mission is to make high-quality, culturally inspired fashion accessible and affordable, connecting people to the beauty of Nigerian traditions through timeless designs.</p>
+          </div>
+      </div>
+
+      <div className=' text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className=' text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className=' text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className=' text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+          </div>
+      </div>
+
+      <NewsletterBox/>
+      
+    </div>
+  )
+}
+
+export default About
